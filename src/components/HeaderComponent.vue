0e5cc49cf80c67ea6cd2c7fa734d3fe5ecc9612c
EliaBrambilla <elia.brambilla@icloud.com>
2022-11-28 11:00:16 +0100

    aggiorno components

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <Header>
+      <img src="../assets/logo.png" alt="">
+    </Header>
+  </template>
+  
+  <script>
+  export default {
+      name:'HeaderComponent',
+  }
+  </script>
+  
+  <style lang="scss" scoped>
+  *{
+      margin: 0 auto;
+  }
+  header {
+      height: 50px;
+      background-color: hsl(210deg 21% 23%);
+  }
+  img {
+      height: 50%;      
+      padding: 1% 2%;
+  }
+  </style>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>Main</div>
+</template>
+
+<script>
+export default {
+    name:'MainComponent',
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
